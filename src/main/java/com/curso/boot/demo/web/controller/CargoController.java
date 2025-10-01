f950436cc1278915755959e0e4285e86e205dfca
WERSINGTON DOS SANTOS SILVA SACRAMENTO <wersington.sssacrame@senacsp.edu.br>
2025-10-01 19:45:04 -0300

    novo

---
+++ src/main/java/com/curso/boot/demo/web/controller/CargoController.java
@@ -0,0 +1,19 @@
+package com.curso.boot.demo.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cargos")
+public class CargoController {
+
+    @GetMapping("/cadastrar")
+    public String cadastrar(){
+        return "/cargos/cadastrar";
+    }
+    @GetMapping("/listar")
+    public  String Listar(){
+        return "/cargos/listar";
+    }
+}
