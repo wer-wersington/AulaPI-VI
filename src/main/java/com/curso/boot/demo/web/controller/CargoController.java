ec38f86123d363b20482092f642418a31b2d6175
WERSINGTON DOS SANTOS SILVA SACRAMENTO <wersington.sssacrame@senacsp.edu.br>
2025-10-08 20:10:15 -0300

    Sql

--- src/main/java/com/curso/boot/demo/web/controller/CargoController.java
+++ src/main/java/com/curso/boot/demo/web/controller/CargoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
-
+//
     @GetMapping("/cadastrar")
     public String cadastrar(){
         return "/cargos/cadastrar";
